06f7115d9da55c7218307962a3c70fcc5441a466
hxl6mp <154755499+hxl6mp@users.noreply.github.com>
2024-10-25 19:26:09 +0100

    Update signin.js

--- functions/signin.js
+++ functions/signin.js
@@ -1,4 +1,4 @@
-// signin.js
+
 
 const faunadb = require('faunadb');
 const q = faunadb.query;
@@ -7,15 +7,15 @@ const client = new faunadb.Client({
   secret: process.env.FAUNADB_SECRET,
 });
 
-// Function to handle sign-in
+
 const signin = async (username, password) => {
   try {
-    // Query the user from FaunaDB
+   
     const result = await client.query(
       q.Get(q.Match(q.Index('users_by_username'), username))
     );
 
-    // Check if the password matches
+    
     if (result.data.password === password) {
       return { statusCode: 200, body: JSON.stringify({ message: 'Login successful.' }) };
     } else {
@@ -26,7 +26,7 @@ const signin = async (username, password) => {
   }
 };
 
-// Exports the signin function for use in Netlify Functions
+
 exports.handler = async (event) => {
   const { username, password } = JSON.parse(event.body);
 
